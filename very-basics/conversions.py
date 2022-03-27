2e3fac3d8ff2b881489fe1a9a6f7792a5cd842b2
viraganna <91409684+viraganna@users.noreply.github.com>
2022-03-27 23:21:14 +0200

    Update and rename conversions to conversions.py

---
+++ very-basics/conversions.py
@@ -0,0 +1,16 @@
+a = input("a? ")
+b = input("b? ")
+ 
+print("a+b =", a + b)
+
+
+a = input("a? ")
+b = input("b? ")
+
+print("a+b =", int(a) + int(b))
+
+
+a = int(input("a?"))
+b = int(input("b?"))
+ 
+print("a+b =", a+b)
