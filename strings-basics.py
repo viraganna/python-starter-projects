0e87ad9f9d5203c204fa0b2d7fbba632b97e4e05
virag anna <88virag@gmail.com>
2022-03-28 19:25:34 +0200

    search in text, turn text into upper case

---
+++ strings-basics.py
@@ -0,0 +1,10 @@
+home_school = "Python lecture"
+print(home_school.upper())
+print(home_school)
+
+
+home_school = "Python lecture"
+print(home_school.find("y"))
+
+home_school = "Python lecture"
+print("Python" in home_school)
