cdbadd4fb3c943921afd26747746a4dd5d164eb8
virag anna <88virag@gmail.com>
2022-03-28 19:00:36 +0200

    did basic data type conversions

---
+++ very-basics/type-converions-birth-year.py
@@ -0,0 +1,3 @@
+birth_year = input("Enter your birth year: ")
+age = 2022 - int(birth_year)
+print(age)
---
+++ very-basics/type-conversions-mini-calculator.py
@@ -0,0 +1,12 @@
+"""
+Inputs for calculator:
+
+First: 20
+Second: 10.1
+Sum:30.1
+"""
+
+first = float(input("First: "))
+second = float(input("Second: "))
+
+print("Sum: " , first + second)
