fe6214c9689db2df7bdcc8e9852429d88eba9cc7
virag anna <88virag@gmail.com>
2022-03-30 14:18:48 +0200

    tried basic logical expressions and operators

---
+++ very-basics/operators-and-logial-expressions.py
@@ -0,0 +1,29 @@
+print(10 / 3)
+print(10 // 3)
+print(10 % 3) #remainder after division
+print(10 * 3)
+print(10 **3)
+
+
+x = 10
+x = x + 3
+print(x)
+
+x = 10
+x +=3
+print(x)
+
+
+x = 2 > 1
+print(x)
+
+
+price = 25
+print(price > 23)
+print(price > 25 and price < 30)
+print(price > 23 and price < 30)
+print(price > 25 or price < 30 )
+print(not price > 26)
+
+
+
